99112646cc92650a103ae7658eb7313bcdf71c9c
ABSQUARE1 <yusufolatuji53@gmail.com>
2023-04-25 09:58:55 +0000

    client

---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const url = 'http://ec2-3-86-177-222.compute-1.amazonaws.com';
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
